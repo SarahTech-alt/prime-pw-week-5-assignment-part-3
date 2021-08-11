4f1fdb144b24eb4f0bab57b6f7081c8461f3c10f
Sarah Fuoss <sfuoss@gmail.com>
2021-08-11 12:24:40 -0500

    added comments to function outputs

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,46 +1,51 @@
 console.log('***** Music Collection *****');
 
 //Creating an array to hold the collection
-let collection = [];
+let collection1 = [];
 
 // First approach to a function that adds items to the collection
 
-function addToCollection (title,artist,yearPublished){
-  collection.push(collection.title = title);
-  collection.push(collection.artist = artist);
-  collection.push(collection.yearPublished = yearPublished);
+function addToCollection1 (title,artist,yearPublished){
+  collection1.push(collection1.title = title);
+  collection1.push(collection1.artist = artist);
+  collection1.push(collection1.yearPublished = yearPublished);
+  return collection1;
 }
 
-addToCollection('Who',"What",98);
-addToCollection("The Bobs","Yeahness", 67);
-console.log(collection);
+addToCollection1('Who',"What",98); //adding first album to collection1
+addToCollection1("The Bobs","Yeahness", 67); //adding second album to collection1
+console.log(collection1); //returns array of an object with properties
+console.log(collection1.title); //returns the title of the second album added
 
 
-/* Second approach to a function that adds items to the collection
+//Second approach to a function that adds items to the collection
+let collection2 = [];
 
-  function addToCollection(title,artist,yearPublished){
-  collection = {
+  function addToCollection2(title,artist,yearPublished){
+  collection2 = {
     title:title,
     artist:artist,
     yearPublished:yearPublished
   };
+  return collection2;
 }
 
-addToCollection('Who',"What",98);
-addToCollection("The Bobs","Yeahness", 67);
-console.log(collection);
-*/
+addToCollection2('Who',"What",98); //adding first album to collection2
+addToCollection2("The Bobs","Yeahness", 67); //adding second album to collection2
+console.log(collection2); //returns object with properties, only includes last addition
 
 
-//Created a function to add items to the collection
+//Third approach to a function that adds items to the collection
+let collection3 = [];
 
-function addToCollection(a,b,c){
-  collection.title = collection.push(a);
-  collection.artist = collection.push(b);
-  collection.yearPublished = collection.push(c);
-  return collection;
+function addToCollection3(a,b,c){
+  collection3.title = collection3.push(a);
+  collection3.artist = collection3.push(b);
+  collection3.yearPublished = collection3.push(c);
+  return collection3;
 }
 
-addToCollection('Who',"What",98);
-addToCollection("The Bobs","Yeahness", 67);
-console.log(collection);
+addToCollection3('Who',"What",98);
+addToCollection3("The Bobs","Yeahness", 67);
+console.log(collection3); //returns array with values added, lists properties as numbers
+console.log(collection3.title); //returns the number 4
