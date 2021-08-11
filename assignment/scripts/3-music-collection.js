96784bcfb9331d0873a29cf9ca240da59feb7d4f
Sarah Fuoss <sfuoss@gmail.com>
2021-08-11 11:47:28 -0500

    Created addtoCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,3 +2,14 @@ console.log('***** Music Collection *****');
 
 //Creating an array to hold the collection
 let collection = [];
+
+//Created a function to add items to the collection
+function addToCollection (title,artist,yearPublished){
+  collection.push(collection.title = title);
+  collection.push(collection.artist = artist);
+  collection.push(collection.yearPublished = yearPublished);
+}
+
+addToCollection('Who',"What",98);
+addToCollection("The Bobs","Yeahness", 67);
+console.log(collection);
