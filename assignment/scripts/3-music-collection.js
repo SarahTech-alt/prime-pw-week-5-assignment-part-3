dd1b6e74aee82f746088e5ce9c1320898b2a1046
Sarah Fuoss <sfuoss@gmail.com>
2021-08-11 12:11:32 -0500

    Testing different function approaches

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,11 +3,42 @@ console.log('***** Music Collection *****');
 //Creating an array to hold the collection
 let collection = [];
 
-//Created a function to add items to the collection
+// First approach to a function that adds items to the collection
+
 function addToCollection (title,artist,yearPublished){
   collection.push(collection.title = title);
   collection.push(collection.artist = artist);
   collection.push(collection.yearPublished = yearPublished);
+}
+
+addToCollection('Who',"What",98);
+addToCollection("The Bobs","Yeahness", 67);
+console.log(collection);
+
+
+/* Second approach to a function that adds items to the collection
+
+  function addToCollection(title,artist,yearPublished){
+  collection = {
+    title:title,
+    artist:artist,
+    yearPublished:yearPublished
+  };
+}
+
+addToCollection('Who',"What",98);
+addToCollection("The Bobs","Yeahness", 67);
+console.log(collection);
+*/
+
+
+//Created a function to add items to the collection
+
+function addToCollection(a,b,c){
+  collection.title = collection.push(a);
+  collection.artist = collection.push(b);
+  collection.yearPublished = collection.push(c);
+  return collection;
 }
 
 addToCollection('Who',"What",98);
